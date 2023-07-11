237d73f17fb89dfe6c30d010cc00ec1d9e905db8
Ngando Epoh Herve <epohherve63@gmail.com>
2023-07-11 04:53:43 +0100

    Update mario_database.sql

--- mario_database.sql
+++ mario_database.sql
@@ -48,7 +48,10 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.characters (
-    character_id integer NOT NULL
+    character_id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    homeland character varying(60),
+    favorite_color character varying(30)
 );
 
 
@@ -77,6 +80,43 @@ ALTER SEQUENCE public.characters_character_id_seq OWNED BY public.characters.cha
 
 
 --
+-- Name: more_info; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.more_info (
+    more_info_id integer NOT NULL,
+    birthday date,
+    height integer,
+    weight numeric(4,1),
+    character_id integer NOT NULL
+);
+
+
+ALTER TABLE public.more_info OWNER TO freecodecamp;
+
+--
+-- Name: more_info_more_info_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.more_info_more_info_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.more_info_more_info_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: more_info_more_info_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.more_info_more_info_id_seq OWNED BY public.more_info.more_info_id;
+
+
+--
 -- Name: characters character_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
 --
 
@@ -84,16 +124,80 @@ ALTER TABLE ONLY public.characters ALTER COLUMN character_id SET DEFAULT nextval
 
 
 --
+-- Name: more_info more_info_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.more_info ALTER COLUMN more_info_id SET DEFAULT nextval('public.more_info_more_info_id_seq'::regclass);
+
+
+--
 -- Data for Name: characters; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.characters VALUES (2, 'Luigi', 'Mushroom Kingdom', 'Green');
+INSERT INTO public.characters VALUES (3, 'Peach', 'Mushroom Kingdom', 'Pink');
+INSERT INTO public.characters VALUES (7, 'Yoshi', 'Dinosaur Land', 'Green');
+INSERT INTO public.characters VALUES (6, 'Daisy', 'Sarasaland', 'Orange');
+INSERT INTO public.characters VALUES (1, 'Mario', 'Mushroom Kingdom', 'Red');
+INSERT INTO public.characters VALUES (4, 'Toad', 'Mushroom Kingdom', 'Blue');
+INSERT INTO public.characters VALUES (5, 'Bowser', 'Koopa Kingdom', 'Yellow');
+
+
+--
+-- Data for Name: more_info; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.more_info VALUES (1, '1981-07-09', 155, 64.5, 1);
+INSERT INTO public.more_info VALUES (2, '1983-07-14', 175, 48.8, 2);
+INSERT INTO public.more_info VALUES (3, '1985-10-18', 173, 52.2, 3);
+INSERT INTO public.more_info VALUES (4, '1950-01-10', 66, 35.6, 4);
+INSERT INTO public.more_info VALUES (5, '1990-10-29', 258, 300.0, 5);
 
 
 --
 -- Name: characters_character_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.characters_character_id_seq', 1, false);
+SELECT pg_catalog.setval('public.characters_character_id_seq', 7, true);
+
+
+--
+-- Name: more_info_more_info_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.more_info_more_info_id_seq', 5, true);
+
+
+--
+-- Name: characters characters_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.characters
+    ADD CONSTRAINT characters_pkey PRIMARY KEY (character_id);
+
+
+--
+-- Name: more_info more_info_character_id_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_character_id_key UNIQUE (character_id);
+
+
+--
+-- Name: more_info more_info_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_pkey PRIMARY KEY (more_info_id);
+
+
+--
+-- Name: more_info more_info_character_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.more_info
+    ADD CONSTRAINT more_info_character_id_fkey FOREIGN KEY (character_id) REFERENCES public.characters(character_id);
 
 
 --
